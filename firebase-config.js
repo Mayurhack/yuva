75c2af3b282ca33688aae9505017dd99adaabba6
Mayurhack <mayurhack@users.noreply.github.com>
2026-07-23 17:30:12 +0530

    deploy: include firebase-config.js in version control

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+// Local Firebase settings. Paste your config credentials below.
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDVpM4WAUe-vrUUdA_5l2Am_uR89bUbba8",
+
+  authDomain: "yuva-sangam-a0e8f.firebaseapp.com",
+
+  projectId: "yuva-sangam-a0e8f",
+
+  storageBucket: "yuva-sangam-a0e8f.firebasestorage.app",
+
+  messagingSenderId: "979878104862",
+
+  appId: "1:979878104862:web:30db5b224afda814c3430f",
+
+  measurementId: "G-2QTLJK8TF5"
+
+};
